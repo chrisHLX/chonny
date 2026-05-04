1d819a1eec08383690168f75bbbcf557047d5e4a
chrisHLX <christian1.lee90@gmail.com>
2026-05-04 19:44:46 +1000

    added cannonical

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name', 'Mindcollector') }}</title>
+        <link rel="canonical" href="https://mindcollector.com/">
         @vite(['resources/css/app.css', 'resources/js/app.js'])
         @livewireStyles
     </head>
