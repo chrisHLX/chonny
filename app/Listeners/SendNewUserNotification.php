322b788593dcbb9ad57b3b666c4f78621e17c078
chrisHLX <christian1.lee90@gmail.com>
2026-07-01 09:51:37 +1000

    500 error fix

--- app/Listeners/SendNewUserNotification.php
+++ app/Listeners/SendNewUserNotification.php
@@ -4,9 +4,10 @@ namespace App\Listeners;
 
 use App\Mail\NewUserRegistered;
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
 
-class SendNewUserNotification
+class SendNewUserNotification implements ShouldQueue
 {
     public function handle(Registered $event): void
     {
